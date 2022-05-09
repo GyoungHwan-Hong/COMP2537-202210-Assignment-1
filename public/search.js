287f1762b4c643ad2b6ad9eea3cba5a524cf1b99
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-05-08 21:06:50 -0700

    Add Search button.

--- public/search.js
+++ public/search.js
@@ -1,14 +1,20 @@
 
-type_g
+type_g = ""
 
 function processPokemonResp(data) {
-    for (i=0; i < data.types.length; i++)
-        if(data.types == type_g)
-            $("main").html(data.id)
+
+    for (i=0; i < data.types.length; i++)      //for (x in data.types)
+        if(data.types[i].type.name == type_g)
+            $("main").append("<p>" + data.id + "</p>")
 }
 
 
 function display(type_){
+
+    type_g = type_
+
+    $("main").empty();
+
     type_g = type_
 
     for (i = 1 ; i < 100; i++) {
@@ -24,11 +30,11 @@ function display(type_){
 
 function setup() {
 
-    diplay(poke_type = $("#poke_type option:selected").val())
+    display($("#poke_type option:selected").val())
     // display all the grass type pokemon
     $("#poke_type").change(() => {
         poke_type = $("#poke_type option:selected").val();
-        alert(poke_type)
+        display($("#poke_type option:selected").val())
     })
 }
 
